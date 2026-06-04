7b7fe4cb0c5621cc2fd98c6b5d1558f2e0af3d02
TSI-yoontae <yoontaeResearch@gmail.com>
2026-06-05 03:13:17 +0900

    Update Home.js

--- js/tabs/Home.js
+++ js/tabs/Home.js
@@ -16,7 +16,7 @@ const ResearchTopicsSection = () => {
         {
             id: 'market',
             title: 'AI in Market',
-            items: ['Optimal Betting (Mainly Polymarket)', "Dark Pool" ,'Sport Science'],
+            items: ['Optimal Betting (Mainly Polymarket)', "Dark Pool", "Limit Order Book" ,'Sport Science'],
         },
         {
             id: 'foundation',

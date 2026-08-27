de13ecf23cee73af8a924d2731e939b2b33bb5af
TSI-yoontae <yoontaeResearch@gmail.com>
2026-08-27 17:37:29 +0900

    Update Project.js

--- js/tabs/Project.js
+++ js/tabs/Project.js
@@ -1,7 +1,21 @@
 window.ProjectTabContent = () => {
     const projects = [
-{
-            title: '글로컬R&D',
+        {
+            title: '글로벌데이터리더양성사업 (with National University of Singapore)',
+            year: 2026,
+            role: '참여연구원(세부책임)',
+            organization: 'National Research Foundation of Korea (NRF)',
+            period: '2026.09 - Present',
+        },
+        {
+            title: '기업 공시자료의 한계가치에 관한 연구',
+            year: 2026,
+            role: '책임연구자',
+            organization: '한국금융학회',
+            period: '2026.09 - Present',
+        },
+        {
+            title: '글로컬 R&D (with University of Oxford)',
             year: 2026,
             role: '책임연구원',
             organization: 'National Research Foundation of Korea (NRF)',
